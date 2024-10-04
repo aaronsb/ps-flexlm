82f208368f38052d2fae22fa585d935c1e02465e
Aaron Bockelie <aaronsb@gmail.com>
2024-10-04 17:42:51 -0500

    updated code and made it a little friendlier to read.

--- README.md
+++ README.md
@@ -1,14 +1,105 @@
 # ps-flexlm
-Powershell FlexLM wrapper
+# Powershell FlexLM Wrapper
 
-This is a small Powershell wrapper to make it easier to get structured license usage data out of a given FlexLM server. I've been trying to recreate the debug log for data, but found that restarts and other environmental interruptions can create a flawed dataset.
+This repository contains a Powershell wrapper designed to simplify the process of retrieving structured license usage data from a FlexLM server. 
 
-Flexera also helpfully offers the choice to *encrypt* the logging data by the vendor's choice in their implementation. That way you can simply purchase one of Flexera's highly coveted log interpetation tools.
+## Overview
 
-Instead, I'll try polling the service directly and just parse all the content.
+FlexLM servers can sometimes produce flawed datasets due to restarts and other environmental interruptions. Additionally, Flexera offers the option to encrypt logging data, which can only be decrypted using their proprietary tools. This wrapper aims to bypass these issues by polling the service directly and parsing the content.
 
-There's a couple other FlexLM parsers out there, notibly written in Python, but I thought one that uses Powershell would be a little more my speed.
+## Features
 
-Additionally, there is an example to export at time of execution, the flexlm data to elasticsearch as a set of usage terms. There is more data available in the returned objects than is pushed into ELS, but with a little extra work you could make that data set relatively rich.
+- **Direct Polling**: Bypasses the need for decrypted log files by polling the FlexLM service directly.
+- **Powershell Implementation**: A Powershell-based solution for those who prefer it over other languages like Python.
+- **Elasticsearch Export**: Includes an example for exporting FlexLM data to Elasticsearch as a set of usage terms. The returned objects contain more data than what is pushed into Elasticsearch, allowing for further customization.
 
-Now that I am re-reading through my code comments, I seem to have been a little angry when I wrote the core functions. 
+## Motivation
+
+While there are other FlexLM parsers available, most are written in Python. This project provides a Powershell alternative that may be more suitable for users comfortable with that environment.
+
+## Usage
+
+### Prerequisites
+
+- Powershell 5.1 or later
+- FlexLM server
+- `flexlm_monitor.xml` configuration file
+
+### Setup
+
+To get started with the Powershell FlexLM Wrapper, follow these steps:
+
+1. **Clone the repository:**
+    First, you need to clone the repository to your local machine. Open your terminal and run the following commands:
+    ```sh
+    git clone https://github.com/yourusername/ps-flexlm.git
+    cd ps-flexlm
+    ```
+
+2. **Configuration File:**
+    Ensure that the `flexlm_monitor.xml` configuration file is in the same directory as the script. This file contains the necessary configuration settings for the FlexLM services you want to monitor. If you don't have this file, you may need to create it based on the example provided in the repository or obtain it from your FlexLM server administrator.
+
+3. **FlexLM Binary:**
+    Make sure the `lmstat` binary is available on your system. This binary is required to communicate with the FlexLM server and retrieve license usage data. The `lmstat` binary is typically included with the FlexLM server installation. Ensure that the directory containing `lmstat` is included in your system's PATH environment variable, or provide the full path to the binary in your script.
+
+4. **Powershell Version:**
+    Verify that you have Powershell 5.1 or later installed on your system. You can check your Powershell version by running the following command in your Powershell terminal:
+    ```powershell
+    $PSVersionTable.PSVersion
+    ```
+    If you need to install or update Powershell, follow the instructions on the [official Powershell documentation](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell).
+
+By following these steps, you will have the necessary setup to use the Powershell FlexLM Wrapper and start retrieving license usage data from your FlexLM server.
+
+### Functions
+
+#### Get-FlexLMStat
+
+Retrieves the status of licenses from the FlexLM server.
+
+**Usage:**
+```powershell
+Get-FlexLMStat -Port <PortNumber> -LMHost <Hostname>
+```
+
+#### Get-FlexLMServices
+
+Lists all FlexLM services defined in the `flexlm_monitor.xml` file.
+
+**Usage:**
+```powershell
+Get-FlexLMServices
+```
+
+#### Get-FlexLMStatHost
+
+Retrieves the status of licenses for a specific service.
+
+**Usage:**
+```powershell
+Get-FlexLMStatHost -ServiceName <ServiceName>
+```
+
+#### Export-FlexLMFeaturesToELS
+
+Exports the FlexLM feature data to an Elasticsearch instance.
+
+**Usage:**
+```powershell
+Export-FlexLMFeaturesToELS -ServiceName <ServiceName> [-doit] [-urlroot <URLRoot>]
+```
+
+### Example
+
+To retrieve the status of licenses from a FlexLM server running on port 27000 at `flexlm.example.com`:
+```powershell
+Get-FlexLMStat -Port 27000 -LMHost flexlm.example.com
+```
+
+To export the feature data to Elasticsearch:
+```powershell
+Export-FlexLMFeaturesToELS -ServiceName MyService -doit
+```
+
+This will push the data to the Elasticsearch instance defined in the `flexlm_monitor.xml` file.
+
--- flexlmtools.ps1
+++ flexlmtools.ps1
@@ -1,38 +1,58 @@
-function Get-FlexLMStat
-{param ([int]$Port,$LMHost)
+<#
+.SYNOPSIS
+    Retrieves the status of licenses from the FlexLM server.
+
+.DESCRIPTION
+    The Get-FlexLMStat function polls the FlexLM server to retrieve the status of licenses. 
+    It processes the raw data returned by the server and structures it into a more readable format.
+    Note: The lmstat utility is required to pull the data from the FlexLM server. Ensure that lmstat is installed and accessible in your system's PATH.
+
+.PARAMETER Port
+    The port number on which the FlexLM server is running.
+
+.PARAMETER LMHost
+    The hostname of the FlexLM server.
+
+.EXAMPLE
+    Get-FlexLMStat -Port 27000 -LMHost flexlm.example.com
+
+    This command retrieves the status of licenses from the FlexLM server running on port 27000 at flexlm.example.com.
+
+.NOTES
+    Ensure that the flexlm_monitor.xml configuration file is in the same directory as the script.
+
+.LINK
+    https://github.com/aaronsb/ps-flexlm
+#>
+function Get-FlexLMStat {
+    param ([int]$Port, $LMHost)
     $erroractionpreference = "SilentlyContinue"
     #extracting this from the toolbox environment this used to live in so it works "standalone"..ish.
-    $Config = [xml](gc ("flexlm_monitor.xml"))
-    function PrivGetRegex
-    {param($FilterName)
-        $config.PSFlexLM.RegExLibrary.Filter | ?{$_.id -eq $FilterName} | %{$_.'#text'}
+    $Config = [xml](Get-Content ("flexlm_monitor.xml"))
+    function PrivGetRegex {
+        param($FilterName)
+        $config.PSFlexLM.RegExLibrary.Filter | Where-Object { $_.id -eq $FilterName } | ForEach-Object { $_.'#text' }
     }
-    function PrivProcessLMDateString
-    {param ($LMDateString)
-        if (!$LMDateString)
-        {
+    function PrivProcessLMDateString {
+        param ($LMDateString)
+        if (!$LMDateString) {
             $DateObject = ""
         }
-        else
-        {
+        else {
             $rgxFloatSeatDateTime = '(^\w+) (\w+)\/(\w+) (\w+:\w+)'
-            #flexlm hates dates that make sense. What the hell kind of date is 1-jan-0? Oh, that's right. A non-expiring license date.
-            if ($LMDateString -eq "1-jan-0")
-            {
+            # FlexLM uses unconventional date formats. The date "1-jan-0" represents a non-expiring license.
+            if ($LMDateString -eq "1-jan-0") {
                 $DateObject = Get-Date "1/1/2000"
             }
-            else
-            {
-                try 
-                {
+            else {
+                try {
                     $DateObject = [datetime]$LMDateString
                 }
-                catch
-                {
-                    #Hey, we don't need to include the year!
-                            #At least they use 24 hour format for the time.
-                            #I put this up here as it's own ugly thing to make it a little more readable.
-                    $DateRaw = ($LMDateString | select-string $rgxFloatSeatDateTime).Matches.Groups.Value
+                catch {
+                    # Note: The year is not required in the date format.
+                    # The time is in 24-hour format for consistency.
+                    # This section is separated for clarity and readability.
+                    $DateRaw = ($LMDateString | Select-String $rgxFloatSeatDateTime).Matches.Groups.Value
                     $DateObject = Get-Date (("{0:D2}" -f [int]$DateRaw[2]) + "/" + ("{0:D2}" -f [int]$DateRaw[3]) + "/" + (Get-Date).Year + " " + $DateRaw[4])    
                 }
             }
@@ -40,125 +60,119 @@ function Get-FlexLMStat
         $DateObject
     }
 
-    #region call lmstat and get it's text body back
-    $LMStatData = iex ($config.PSFlexLM.Configuration.LMUtilPath + " lmstat -c $Port@$LMHost -a")
+    #region Execute lmstat command to retrieve the license status information from the FlexLM server
+    $LMStatData = Invoke-Expression ($config.PSFlexLM.Configuration.LMUtilPath + " lmstat -c $Port@$LMHost -a")
     
-    #region define all the regex objects. maybe roll this into the filter operations themselves in the near future
+    #region Define all the regular expression objects. Consider integrating these directly into the filter operations for improved maintainability in the future.
     $rgxLicenseServer = (PrivGetRegex ServerStatus)
     $rgxFeatureUse = (PrivGetRegex FeatureUseOverview)
     $rgxBulkFloatData = (PrivGetRegex FloatingLicensePrefilter)
     $rgxFloatFeatureParse = (PrivGetRegex FloatingLicenseFeatureSummary)
     $rgxFloatSeatConsumption = (PrivGetRegex FloatingLicenseFeatureDetail)
     
-    $ServerStatus = $LMStatData | select-string $rgxLicenseServer
-    $FeatureStatus = $LMStatData | select-string $rgxFeatureUse
-    $FloatingFeatureData = ($LMStatData | select-string $rgxBulkFloatData).Line
+    $ServerStatus = $LMStatData | Select-String $rgxLicenseServer
+    $FeatureStatus = $LMStatData | Select-String $rgxFeatureUse
+    $FloatingFeatureData = ($LMStatData | Select-String $rgxBulkFloatData).Line
     
     #region build server status hashtable
-    $StatusHash = [PSCustomObject]@{"Hostname" = $ServerStatus.Matches.Groups[1].Value;"Status" = $ServerStatus.Matches.Groups[2].Value}
+    $StatusHash = [PSCustomObject]@{"Hostname" = $ServerStatus.Matches.Groups[1].Value; "Status" = $ServerStatus.Matches.Groups[2].Value }
 
     #region build featurehashtable
     $FeatureHash = New-Object System.Collections.Generic.List[System.Object]
-    foreach ($feature in $FeatureStatus)
-    {
+    foreach ($feature in $FeatureStatus) {
         $FeatureHash.Add([PSCustomObject]@{"FeatureName" = $feature.Matches.Groups[1].Value;
-                            "Issued" = $feature.Matches.Groups[2].Value;
-                            "Consumed" = $feature.Matches.Groups[3].Value;
-                            "Available" = ($feature.Matches.Groups[2].Value - $feature.Matches.Groups[3].Value);
-                            "FloatingFeatureStatus" = New-Object System.Collections.Generic.List[System.Object]})
+                "Issued"                                 = $feature.Matches.Groups[2].Value;
+                "Consumed"                               = $feature.Matches.Groups[3].Value;
+                "Available"                              = ($feature.Matches.Groups[2].Value - $feature.Matches.Groups[3].Value);
+                "FloatingFeatureStatus"                  = New-Object System.Collections.Generic.List[System.Object]
+            })
     }
 
 
 
     #region build floating feature detail hash table
     $FloatingFeatureHash = New-Object System.Collections.Generic.List[System.Object]
-    $i=0
+    $i = 0
     do {
         do {
-                if ($floatingFeatureData[$i].IndexOf("    ") -eq -1)
-                {   
-                    $header = $null
-                    $guid = [guid]::NewGuid()
-                    #-1 means header. this is a header line. Parse data.
-                    $header = $floatingFeatureData[$i] | select-string $rgxFloatFeatureParse
+            if ($floatingFeatureData[$i].IndexOf("    ") -eq -1) {   
+                $header = $null
+                $guid = [guid]::NewGuid()
+                #-1 means header. this is a header line. Parse data.
+                $header = $floatingFeatureData[$i] | Select-String $rgxFloatFeatureParse
                     
-                    $FloatingFeatureHash.Add([PSCustomObject]@{"guid" = $guid
-                                                "FloatingFeatureName" = $header.Matches.Groups.Value[1];
-                                                "FeatureVersion" = $header.Matches.Groups.Value[2];
-                                                "VendorDaemon" = $header.Matches.Groups.Value[3];
-                                                "FeatureExpiry" = PrivProcessLMDateString $header.Matches.Groups.Value[4];
-                                                "SeatConsumptionData" = New-Object System.Collections.Generic.List[System.Object]})
-                }
-                else
-                {
-                    $data = $null
-                    #0 means data. this is a data line. Parse data.
-                    $data =  $floatingFeatureData[$i] | select-string $rgxFloatSeatConsumption
-                   
-                    #$floatingLicenseUse = $data.Matches.Groups.Value
-                   
-                    $floatingLicenseUse = [PSCustomObject]@{"Username" = $data.Matches.Groups.Value[1];
-                                                            "LicenseLoanedHost" = $data.Matches.Groups.Value[2];
-                                                            "Version" = $data.Matches.Groups.Value[3];
-                                                            "LicenseManagerHost" = $data.Matches.Groups.Value[4];
-                                                            "LicenseManagerHostTCPPort" = $data.Matches.Groups.Value[5];
-                                                            "ThisIsProbablyImportantToo" = $data.Matches.Groups.Value[6];
-                                                            "DateAction" = $data.Matches.Groups.Value[7];
-                                                            "Date" = PrivProcessLMDateString $data.Matches.Groups.Value[8]}
-                                                            
-                    ($FloatingFeatureHash | ?{$guid -eq $_.Guid}).SeatConsumptionData.Add($floatingLicenseUse)
-                }$i++   
+                $FloatingFeatureHash.Add([PSCustomObject]@{"guid" = $guid
+                        "FloatingFeatureName"                     = $header.Matches.Groups.Value[1];
+                        "FeatureVersion"                          = $header.Matches.Groups.Value[2];
+                        "VendorDaemon"                            = $header.Matches.Groups.Value[3];
+                        "FeatureExpiry"                           = PrivProcessLMDateString $header.Matches.Groups.Value[4];
+                        "SeatConsumptionData"                     = New-Object System.Collections.Generic.List[System.Object]
+                    })
             }
-            Until (($FloatingFeatureData[$i].IndexOf("    ") -eq -1) -or $FloatingFeatureData.Length -eq $i)
+            else {
+                $data = $null
+                #0 means data. this is a data line. Parse data.
+                $data = $floatingFeatureData[$i] | Select-String $rgxFloatSeatConsumption
+                   
+                #$floatingLicenseUse = $data.Matches.Groups.Value
+                   
+                $floatingLicenseUse = [PSCustomObject]@{"Username" = $data.Matches.Groups.Value[1];
+                    "LicenseLoanedHost"                            = $data.Matches.Groups.Value[2];
+                    "Version"                                      = $data.Matches.Groups.Value[3];
+                    "LicenseManagerHost"                           = $data.Matches.Groups.Value[4];
+                    "LicenseManagerHostTCPPort"                    = $data.Matches.Groups.Value[5];
+                    "ThisIsProbablyImportantToo"                   = $data.Matches.Groups.Value[6];
+                    "DateAction"                                   = $data.Matches.Groups.Value[7];
+                    "Date"                                         = PrivProcessLMDateString $data.Matches.Groups.Value[8]
+                }
+                                                            
+                    ($FloatingFeatureHash | Where-Object { $guid -eq $_.Guid }).SeatConsumptionData.Add($floatingLicenseUse)
+            }$i++   
         }
+        Until (($FloatingFeatureData[$i].IndexOf("    ") -eq -1) -or $FloatingFeatureData.Length -eq $i)
+    }
     Until ($i -ge [int]$FloatingFeatureData.Length)
 
     #region insert floating features into overall feature state
-    foreach ($FloatingFeature in $FloatingFeatureHash)
-    {
-        foreach ($Feature in $FeatureHash)
-        {
-            if ($FloatingFeature.FloatingFeatureName -eq $Feature.FeatureName)
-            {
-                $Feature.FloatingFeatureStatus.Add(($FloatingFeature | Select-Object -Property FeatureVersion,VendorDaemon,FeatureExpiry,SeatConsumptionData))
+    foreach ($FloatingFeature in $FloatingFeatureHash) {
+        foreach ($Feature in $FeatureHash) {
+            if ($FloatingFeature.FloatingFeatureName -eq $Feature.FeatureName) {
+                $Feature.FloatingFeatureStatus.Add(($FloatingFeature | Select-Object -Property FeatureVersion, VendorDaemon, FeatureExpiry, SeatConsumptionData))
             }
         }
     }
-    @{"FeatureState" = $FeatureHash;"ServerState" = $StatusHash;"FloatingState" = $FloatingFeatureHash}
+    @{"FeatureState" = $FeatureHash; "ServerState" = $StatusHash; "FloatingState" = $FloatingFeatureHash }
     
 }
 
 
-function Get-FlexLMServices
-{
+function Get-FlexLMServices {
     #extracting this from the toolbox environment this used to live in so it works "standalone"..ish.
-    #$Config = [xml](gc ($env:toolboxxml + "\etc\flexlm_monitor.xml"))
-    $Config = [xml](gc ("flexlm_monitor.xml"))
+    #$Config = [xml](Get-Content ($env:toolboxxml + "\etc\flexlm_monitor.xml"))
+    $Config = [xml](Get-Content ("flexlm_monitor.xml"))
     $config.PSFlexLM.Servers.Daemon
 }
 
 
-function Get-FlexLMStatHost
-{param($ServiceName)
+function Get-FlexLMStatHost {
+    param($ServiceName)
     #extracting this from the toolbox environment this used to live in so it works "standalone"..ish.
-    #$Config = [xml](gc ($env:toolboxxml + "\etc\flexlm_monitor.xml"))
-    $Config = [xml](gc ("flexlm_monitor.xml"))
+    #$Config = [xml](Get-Content ($env:toolboxxml + "\etc\flexlm_monitor.xml"))
+    $Config = [xml](Get-Content ("flexlm_monitor.xml"))
 
 
-    foreach ($daemon in $config.PSFlexLM.Servers.Daemon)
-    {
-        if ($daemon.id -match $ServiceName)
-        {
+    foreach ($daemon in $config.PSFlexLM.Servers.Daemon) {
+        if ($daemon.id -match $ServiceName) {
             Get-FlexLMStat -Port $daemon.TCPPort -LMHost $daemon.hostname
         }
         
     }
 }
 
-function Export-FlexLMFeaturesToELS
-{param($serviceName,[switch]$doit,$urlroot = "http://elk.contoso.com:9199")
+function Export-FlexLMFeaturesToELS {
+    param($serviceName, [switch]$doit, $urlroot = "http://elk.contoso.com:9199")
     $StatObject = Get-FlexLMServices $serviceName
-    $Config = [xml](gc ("flexlm_monitor.xml"))
+    $Config = [xml](Get-Content ("flexlm_monitor.xml"))
     $urlroot = $Config.PSFlexLM.Elastic.Host
     
     #mappings
@@ -181,17 +195,16 @@ function Export-FlexLMFeaturesToELS
     }'
     #end mappings
 
-    if (!$StatObject)
-    {
+    if (!$StatObject) {
         write-error "No Stat object found by that name."
         break
     }
     #configure flexlm index in els
     $elk_index = ("flexlm_" + $serviceName)
     $elk_type = "feature"
-    $features = $StatObject.FeatureState | Select-Object "FeatureName","Issued","Consumed","Available"
+    $features = $StatObject.FeatureState | Select-Object "FeatureName", "Issued", "Consumed", "Available"
     $features | Add-Member -MemberType NoteProperty -Name "@timestamp" -value ""
-    $features | %{$_."@timestamp" = $StatObject.ServerState.DateTime}
+    $features | ForEach-Object { $_."@timestamp" = $StatObject.ServerState.DateTime }
     #each feature is a type stored in an index.
 
     #verify index (and mapping exists). If not, create the initial index and the mapping.
@@ -205,8 +218,7 @@ function Export-FlexLMFeaturesToELS
     #reverify index mappings after add.
 
     $els_index_config = Invoke-RestMethod -Method Get -Uri ($urlroot + "/" + $elk_index)
-    if (!$els_index_config.$elk_index.mappings.FeatureName.properties)
-    {
+    if (!$els_index_config.$elk_index.mappings.FeatureName.properties) {
         try {
             write-warning "Existing index found without mappings. Trying to add mappings."
             Invoke-RestMethod -Method PUT -Uri ($urlroot + "/" + $elk_index) -ContentType "application/json" -Body $ELSMappings | Out-Null
@@ -222,20 +234,13 @@ function Export-FlexLMFeaturesToELS
     }
     
     $uri = ($urlroot + "/" + $elk_index + "/" + $elk_type)
-    foreach ($item in $features)
-    {
-        $body = ($item | Select-Object "FeatureName","Issued","Consumed","Available","@timestamp") | ConvertTo-Json -Compress
-        if ($doit)
-        {
-            Invoke-RestMethod -Method Post -Uri $uri -ContentType 'application/json' -Body $body | out-null    
+    foreach ($item in $features) {
+        $body = ($item | Select-Object "FeatureName", "Issued", "Consumed", "Available", "@timestamp") | ConvertTo-Json -Compress
+        if ($doit) {
+            Invoke-RestMethod -Method Post -Uri $uri -ContentType 'application/json' -Body $body | Out-Null    
         }
-        else
-        {
+        else {
             "$body $uri"
         }
     }
 }
-
-
-
-
